776a672815ad0615b057813dc2145abb09b1b8cc
Cristian González <crisgon1065@gmail.com>
2022-06-08 13:33:01 -0600

    Agregar estructuras de codigo en archivos de prueba

--- estructurada.java
+++ estructurada.java
@@ -2,6 +2,13 @@ package codigo;
 import java;
 public class Principal {
     public static void main() {
+        if (a < b) {
+            String hola = "Caderna";
+        } else {
+            int ruta;
+        }
         int ruta1 = 1;
+        System.out.println("hola si");
+        1 + 1;
     }
 }
